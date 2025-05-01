92d195b636fb0a33cc668880d532b2a4a784ed45
snigdh <snigdhkarki@gmail.com>
2025-05-01 06:52:21 +0545

    finalefinale

--- utils/parser.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import os
-
-def extract_zip(file_bytes, extract_to):
-    with open(os.path.join(extract_to, "temp.zip"), "wb") as f:
-        f.write(file_bytes)
-    with zipfile.ZipFile(os.path.join(extract_to, "temp.zip"), 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
